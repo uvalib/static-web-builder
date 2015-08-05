4f0894df7cb1fa3c1c10180844d54bb6d8fcb788
Doug Chestnut <dougchestnut@gmail.com>
2015-08-05 11:05:00 -0400

    Script that gets public Library related groups from myGroup LDAP server, gets members, then gets member listing from the public LDAP server and spits it out to stdout as json

---
+++ getStaffFromLDAP.js
@@ -0,0 +1,67 @@
+var ldap = require('ldapjs'),
+    q = require('q'),
+    assert = require('assert');
+
+var directory = {allMembers:{},
+                 allGroups:{}},
+    uids = [];
+
+var groupClient = ldap.createClient({ url: 'ldap://pitchfork.itc.virginia.edu' }),
+    userClient = ldap.createClient({ url: 'ldap://ldap.virginia.edu'});
+
+// find all the non-private groups that start with Library_
+groupClient.bind("cn=lb-user-svcs,ou="+process.env.MYGROUP_USER,process.env.MYGROUP_PASSWORD,function(err) {
+  assert.ifError(err);
+  var opts = {
+    filter: "(&(cn=Library_*)(!(uvaPrivateGroup=yes)))",
+    attributes: ['cn','memberUid'],
+    scope: 'sub'
+  };
+  groupClient.search("ou=Groups,o=University of Virginia,c=US", opts, function(err,res){
+    assert.ifError(err);
+    res.on('searchEntry', function(entry){
+      processGroup(entry.object);
+    });
+    res.on('end', function(result){
+      harvestUsers();
+    });
+  });
+});
+
+var processGroup = function(group) {
+  if (group.memberUid) {
+    var members = (typeof group.memberUid === 'string')? [group.memberUid]:group.memberUid;
+    members.forEach(function(uid){ uids.push(uid); });
+    directory.allGroups[group.cn] = {mambers: members };
+  }
+};
+
+var harvestUsers = function(index) {
+
+  if (!index) index=0;
+  if (index < uids.length)
+    getUser(uids[index]).then(function(){ harvestUsers(index+1); });
+  else {
+    console.log( JSON.stringify(directory) );
+    process.exit();
+  }
+}
+
+var getUser = function(uid) {
+  var deferred = q.defer(),
+      opts = {
+        filter: "uid="+uid,
+//        attributes: [],
+        scope: 'sub'
+      };
+  userClient.search("o=University of Virginia,c=US",opts,function(err,res){
+    assert.ifError(err);
+    res.on('searchEntry', function(entry){
+      directory.allMembers[uid]=entry.object;
+    });
+    res.on('end', function(result){
+      deferred.resolve('done');
+    });
+  });
+  return deferred.promise;
+};
