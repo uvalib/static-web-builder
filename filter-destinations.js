860ce57b0d411006c9f34412e68bda871fcb67d8
Doug Chestnut <dougchestnut@gmail.com>
2017-03-02 14:29:25 -0500

    added in filter to firebase for destination type

---
+++ filter-destinations.js
@@ -0,0 +1,94 @@
+var _ = require('lodash');
+var items = require('./destinations.json');
+var transform = {
+  nid: {
+    newName: 'id',
+    props: {value:String}
+  },
+  uuid: {
+    props: {value:String}
+  },
+  title: {
+    props: {value: String}
+  },
+  field_library: {
+    newName: "library",
+    props: {target_id:{newName:'id',type:Number},target_uuid:{newName:'uuid',type:String}}
+  },
+  field_area: {
+    newName: "area",
+    props: {value:String}
+  },
+  field_callnumber_end: {
+    newName: "callEnd",
+    props: {value:String}
+  },
+  field_callnumber_start: {
+    newName: "callStart",
+    props: {value:String}
+  },
+  field_callnumber_key: {
+    newName: "callKey",
+    props: {value:String}
+  },
+  field_directions: {
+    newName: "directions",
+    props: {value:String}
+  },
+  field_floor: {
+    newName: "floor",
+    props: {value:String}
+  },
+  field_format_key: {
+    newName: "formatKey",
+    props: {value:String}
+  },
+  field_location_key: {
+    newName: "locationKey",
+    props: {value:String}
+  }
+}; 
+
+var processProp = function(key, trDef, prop){
+  var type = trDef.type || trDef;
+  var newKey = trDef.newName || key;
+  if (!prop) {
+    return {};
+  } else if (type == String) {
+    return {[newKey]:prop[key]};
+  } else if (type == Boolean) {
+    return {[newKey]:(prop[key]=="0")?false:true};
+  } else if (type == Number) {
+    return {[newKey]:Number(prop[key])};
+  } else {
+    return {};
+  }
+};
+ 
+items = _.map(items, function(item){
+  newProps = {};
+  for (allowedPropName in transform) {
+    if (item.hasOwnProperty(allowedPropName)) {
+      var proptr = transform[allowedPropName];
+      newPropName = proptr['newName'] || allowedPropName; 
+      newProps[newPropName] = []
+      for (i=0; i<item[allowedPropName].length; i++) {
+        newProps[newPropName][i] = {};
+        for (pkey in proptr.props) {
+          newProps[newPropName][i] = Object.assign(newProps[newPropName][i], processProp(pkey, proptr.props[pkey], item[allowedPropName][i]) ); 
+        } 
+      }
+      if (newProps[newPropName].length == 1) {
+        newProps[newPropName] = newProps[newPropName][0];
+        // If only one property just use the value insead of the whole object
+        if (Object.keys(proptr.props).length == 1) {
+          newProps[newPropName] = _.values(newProps[newPropName])[0];
+        }
+      }
+    }
+  }
+  return newProps;
+});
+
+//console.log(items);
+console.log(JSON.stringify(items));
