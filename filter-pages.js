3f9463d4469f06571b2dbfb39afcb55497b2b8b5
Doug Chestnut <dougchestnut@gmail.com>
2017-03-03 13:50:48 -0500

    pull pages to api

---
+++ filter-pages.js
@@ -0,0 +1,65 @@
+var _ = require('lodash');
+var items = require('./pages.json');
+var transform = {
+  nid: {
+    newName: 'id',
+    props: {value:String}
+  },
+  uuid: {
+    props: {value:String}
+  },
+  title: {
+    props: {value: String}
+  },
+  body: {
+    props: {value: String}
+  },
+  field_request_paths: {
+    newName: "paths",
+    props: {value: String}
+  }
+};
+
+var processProp = function(key, trDef, prop){
+  var type = trDef.type || trDef;
+  var newKey = trDef.newName || key;
+  if (!prop) {
+    return {};
+  } else if (type == String) {
+    return {[newKey]:prop[key]};
+  } else if (type == Boolean) {
+    return {[newKey]:(prop[key]=="0")?false:true};
+  } else if (type == Number) {
+    return {[newKey]:Number(prop[key])};
+  } else {
+    return {};
+  }
+};
+
+items = _.map(items, function(item){
+  newProps = {};
+  for (allowedPropName in transform) {
+    if (item.hasOwnProperty(allowedPropName)) {
+      var proptr = transform[allowedPropName];
+      newPropName = proptr['newName'] || allowedPropName;
+      newProps[newPropName] = []
+      for (i=0; i<item[allowedPropName].length; i++) {
+        newProps[newPropName][i] = {};
+        for (pkey in proptr.props) {
+          newProps[newPropName][i] = Object.assign(newProps[newPropName][i], processProp(pkey, proptr.props[pkey], item[allowedPropName][i]) );
+        }
+      }
+      if (newProps[newPropName].length == 1) {
+        newProps[newPropName] = newProps[newPropName][0];
+        // If only one property just use the value insead of the whole object
+        if (Object.keys(proptr.props).length == 1) {
+          newProps[newPropName] = _.values(newProps[newPropName])[0];
+        }
+      }
+    }
+  }
+  return newProps;
+});
+
+//console.log(items);
+console.log(JSON.stringify(items));
