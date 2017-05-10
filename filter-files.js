3fdc7bf1b93751efe43aab86fba32de8f3675215
Doug Chestnut <dougchestnut@gmail.com>
2017-05-10 14:43:25 -0400

    start of file handling

---
+++ filter-files.js
@@ -0,0 +1,202 @@
+//var jsonKey = '/Users/dhc4z/Downloads/uvalib-api-firebase-adminsdk-urtjy-b407df0805.json'
+var jsonKey = '/home/bamboo/uvalib-api-firebase-adminsdk-urtjy-b407df0805.json'
+
+var fbadmin = require('firebase-admin'),
+    fbserviceAccount = require(jsonKey),
+    cheerio = require('cheerio'),
+    fs = require('fs'),
+    dataIn = "",
+    moment = require('moment'),
+    imagemin = require('imagemin'),
+    imageminJpegtran = require('imagemin-jpegtran'),
+    imageminPngquant = require('imagemin-pngquant'),    
+    imageminWebp = require('imagemin-webp'),
+    im = require('imagemagick'),
+    gcloud = require('gcloud'),
+    request = require('request');
+
+var storage = gcloud.storage({
+  projectId: 'uvalib-api',
+  keyFilename: jsonKey 
+//  keyFilename: '/home/bamboo/uvalib-api-firebase-adminsdk-urtjy-b407df0805.json'
+});
+
+var bucket = storage.bucket('uvalib-api.appspot.com');
+
+/**
+ * Connect & Setup of Firebase DB
+ */
+fbadmin.initializeApp({
+  credential: fbadmin.credential.cert(fbserviceAccount),
+  databaseURL: "https://uvalib-api.firebaseio.com"
+});
+
+var fbdb = fbadmin.database();
+var filesRef = fbdb.ref('files');
+
+console.log('attempt to get known files'); 
+
+// Get all the known files from our api
+filesRef.once("value", function(knownFiles){
+
+  console.log('file count in db currently '+knownFiles.numChildren());
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8')
+
+  process.stdin.on('data', function(chunk) {
+    dataIn += chunk;
+  });
+
+  // Read in from stdin (from drupal), then parse and start iterating recursivly
+  process.stdin.on('end', function(){
+    $ = cheerio.load(dataIn);
+    var fileRows = $('.views-row').toArray();
+    munchFileRows(fileRows)
+  });
+
+});
+
+var getFilesizeInBytes = function(filename) {
+    const stats = fs.statSync(filename)
+    const fileSizeInBytes = stats.size
+    return fileSizeInBytes
+}
+
+// Todo, skip known files that haven't been changed
+var munchFileRows = function(fileRows){
+  row = $( fileRows.shift() ); 
+  var uuid = getValue(row, 'views-field-uuid');
+  var file = {};
+  file.uuid = uuid;
+  file.name = getValue(row, 'views-field-filename');
+  file.ext = '.'+file.name.replace(/.*\./,'');
+  var href = $(row).find('.views-field-filename .field-content a').attr('href');
+  file.type = getValue(row, 'views-field-filemime');
+  file.created = getTime(row, 'views-field-created');
+  file.changed = getTime(row, 'views-field-changed');
+
+  // download file for further processing
+  download(href, uuid+file.ext, function(){
+    saveToStorage(uuid+file.ext, uuid+file.ext, function(url) {
+      file.origSrc = url;
+      file.origSrcSize = getFilesizeInBytes(uuid+file.ext);
+      //if this is an image get a thumb and tweek the image
+      if (file.type == "image/jpeg" || file.type == "image/png") {
+        console.log('make other versions of the image');
+        processImage(file, function(){ saveMeta(uuid, file, function(){continueProcessing(fileRows);} )});
+      //else just save file meta to firebase and continue
+      } else {
+        saveMeta(uuid, file, function(){continueProcessing(fileRows);});
+      }
+    });
+  });
+}
+
+var saveToStorage = function(fin, name, callback){
+  var inStream = fs.createReadStream(fin);
+  var bucketFile = bucket.file(name);
+  var bucketWriteStream = bucketFile.createWriteStream();
+  inStream.pipe(bucketWriteStream).on('finish', function(){
+    console.log('saved '+name+' to storage bucket');
+    bucketFile.makePublic(function(){
+      callback("https://storage.googleapis.com/uvalib-api.appspot.com/"+name);
+    });
+  }); 
+}
+
+var mkThumb = function(fin, callback){
+  if (fs.existsSync(fin)) {
+    console.log("making thumb out of "+fin);
+    im.resize({srcPath:fin, dstPath:fin+'thumb',  width: 120}, function(err, stdout, stderr){
+      if (err) throw err;
+      callback();
+    });
+  } else { callback(''); }
+}
+
+var processImage = function(file, callback){
+
+  // Optimize a version of this image (if possible)
+  imagemin([file.uuid+file.ext], 'processedImages', {plugins: [imageminJpegtran(), imageminPngquant({quality: '65-80'})]})
+    .then(function(){
+      saveToStorage('processedImages/'+file.uuid+file.ext, file.uuid+'.comp'+file.ext, function(url) {
+        file.compSrc = url;
+        file.compSrcSize = getFilesizeInBytes('processedImages/'+file.uuid+file.ext);
+        mkThumb('processedImages/'+file.uuid+file.ext, function(){
+          saveToStorage('processedImages/'+file.uuid+file.ext+'thumb', file.uuid+'.thumb'+file.ext, function(url){
+            file.thumbSrc = url;
+            file.thumbSrcSize = getFilesizeInBytes('processedImages/'+file.uuid+file.ext+'thumb');
+            
+            // Make a webp image (expermental)
+            var webpconfig = (file.type =="image/jpeg")? imageminWebp({quality: 75}):imageminWebp({lossless: true});
+            imagemin([file.uuid+file.ext], 'processedImages', {plugins: [webpconfig]})
+              .then(function(){
+                if (fs.existsSync('processedImages/'+file.uuid+'.webp')) {
+                  saveToStorage('processedImages/'+file.uuid+'.webp', file.uuid+'.webp', function(url){
+                      file.webpSrc = url;
+                      file.webpSrcSize = getFilesizeInBytes('processedImages/'+file.uuid+'.webp');
+                      mkThumb('processedImages/'+file.uuid+'.webp', function(){
+                        saveToStorage('processedImages/'+file.uuid+'.webp'+'thumb', file.uuid+'.thumb'+'.webp', function(url){
+                          file.webpthumbSrc = url;
+                          file.webpthumbSrcSize = getFilesizeInBytes('processedImages/'+file.uuid+'.webp'+'thumb');
+                          callback();
+                        });
+                      });
+                  });
+                }
+              })
+              .catch(callback);
+
+
+          });
+
+        });
+      }); 
+    })
+    // Something happened just trigger the callback
+    .catch(function(err){
+      console.log(err);
+      callback();
+    });
+}
+
+var saveMeta = function(uuid, file, callback){
+  var fileRef = filesRef.child(uuid);
+  fileRef.set(file, function(){
+    console.log('downloaded file and saved meta for '+uuid);
+    callback(); 
+  });
+}
+
+var continueProcessing = function(fileRows){
+  // continue munching if there are more
+  if (fileRows.length > 0)
+    munchFileRows(fileRows);
+  // or exit
+  else
+    exit();
+}
+
+var getTime = function(context, name){
+  var datestring = getValue(context, name);
+  return moment(datestring, "ddd, MM/DD/YYYY - HH:mm").unix();
+}
+
+var getValue = function(context, name){
+  return $(context).find('.'+name+' .field-content').text().replace(/^\s+|\s+$/g,'');
+}
+
+var exit = function(){
+  fbdb.goOffline();
+  process.exit(0);  
+}
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    if (!res) console.log(err);
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
