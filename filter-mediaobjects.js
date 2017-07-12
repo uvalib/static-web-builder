60b6a358a41776a3e5edff8e66f926d56cf890ae
Doug Chestnut <dougchestnut@gmail.com>
2017-07-12 16:22:19 -0400

    pull media objects to our api

---
+++ filter-mediaobjects.js
@@ -0,0 +1,38 @@
+var jsontr = require('./json-transform.js');
+
+var items = require('./bookplates.json');
+var transform = {
+  nid: {
+    newName: 'id',
+    props: {value:String}
+  },
+  uuid: {
+    props: {value:String}
+  },
+  title: {
+    props: {value: String}
+  },
+  body: {
+    props: {value: String}
+  },
+  field_association: {
+    newName: "association",
+    props: {value: String}
+  },
+  field_attribution: {
+    newName: "attribution",
+    props: {value: String}
+  },
+  field_media_object_image: {
+    newName: "image",
+    publishImage: 'url',
+    props: {alt:String, width:Number, height:Number, url:String,
+            target_uuid:{
+              type: String,
+              newName: "uuid"
+            }
+           }
+  }
+};
+
+console.log( JSON.stringify( jsontr.transform(items,transform) ) );
