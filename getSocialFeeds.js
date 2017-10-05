e69099ef2e7f2d382017f97f692ed72e2e565468
Bamboo <bamboo@bamboo.lib.virginia.edu>
2017-10-05 16:34:09 -0400

    facebook feed holding things up removing for now

--- getSocialFeeds.js
+++ getSocialFeeds.js
@@ -1,8 +1,8 @@
 var agg = require('social-aggregate')
 
 var sources = {
-  facebook: {accessToken: process.env.FACEBOOK_APP_ID+"|"+process.env.FACEBOOK_APP_SECRET,
-             paths: ['/136530559704906/feed']}, 
+//  facebook: {accessToken: process.env.FACEBOOK_APP_ID+"|"+process.env.FACEBOOK_APP_SECRET,
+//             paths: ['/136530559704906/feed']}, 
   twitter:  {consumerKey: process.env.TWITTER_CONSUMER_KEY,
              consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
              accessToken: process.env.TWITTER_ACCESS_TOKEN,
