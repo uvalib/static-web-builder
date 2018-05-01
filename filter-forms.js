d4439c4e9dd22282947ef68d50e805234b453c45
Jack Kelly <jlk4p@virginia.edu>
2018-05-01 17:57:31 -0400

    adjusted forms to read pages feed

--- filter-forms.js
+++ filter-forms.js
@@ -1,29 +1,34 @@
-var exec = require('child_process').exec;
-var formList = require('./forms.json');
-var i = 0;
-var allForms = Array();
+var exec = require('child_process').exec,
+    pageList = require('./pages.json'),
+    _ = require('lodash'),
+    i = 0,
+    allForms = Array();
 
-function processForms() {
+var formList = _.filter(pageList, function(obj) { return obj.field_webform.length > 0;});
+
+function gatherForms() {
   if (i < formList.length) {
-    var cmd = 'wget http://drupal.lib.virginia.edu/webform_rest/'+formList[i].webform[0].target_id+'/elements?_format=json -O frmDetail'+formList[i].webform[0].target_id+'.json'
-    exec(cmd, function (error, stdout, stderr) {
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      } else {
-        var form = {
-          title: formList[i].title[0].value,
-          url: formList[i].webform[0].url,
-          target_id: formList[i].webform[0].target_id,
-          webform: require('./frmDetail'+formList[i].webform[0].target_id+'.json')
-        }
-        allForms[i] = form;
+    if (formList[i].field_webform.length > 0) {
+      var form = {
+        title: formList[i].title[0].value,
+        path: formList[i].field_path[0].value,
+        target_id: formList[i].field_webform[0].target_id
       }
-      i++;
-      processForms();
-    });
+      var cmd = 'wget http://drupal.lib.virginia.edu/webform_rest/'+formList[i].field_webform[0].target_id+'/elements?_format=json -O frmDetail'+formList[i].field_webform[0].target_id+'.json'
+      exec(cmd, function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        } else {
+          form['webform'] = require('./frmDetail'+form.target_id+'.json');
+          allForms[i] = form;
+          i++;
+          gatherForms();
+        }
+      });
+    }
   } else {
     console.log(JSON.stringify(allForms));
   }
 }
 
-processForms();
+gatherForms();
