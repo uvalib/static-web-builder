44fad1b186950ab11efe5af5760029229263a88d
Jack Kelly <jlk4p@virginia.edu>
2018-05-03 16:47:58 -0400

    add deep mapper lodash to remove # from JSON form keys

--- filter-forms.js
+++ filter-forms.js
@@ -6,6 +6,16 @@ var exec = require('child_process').exec,
 
 var formList = _.filter(pageList, function(obj) { return obj.field_webform.length > 0;});
 
+// See https://stackoverflow.com/questions/35055731/how-to-deeply-map-object-keys-with-javascript-lodash
+// for use of mixin to add this deep mapping solution to remove # from keys to get past Firebase issue
+_.mixin({
+    deep: function (obj, mapper) {
+        return mapper(_.mapValues(obj, function (v) {
+            return _.isPlainObject(v) ? _.deep(v, mapper) : v;
+        }));
+    },
+});
+
 function gatherForms() {
   if (i < formList.length) {
     if (formList[i].field_webform.length > 0) {
@@ -19,15 +29,12 @@ function gatherForms() {
         if (error !== null) {
           console.log('exec error: ' + error);
         } else {
-/*          var webform = require('./frmDetail'+form.target_id+'.json');
-          var newWebform = Object();
-          for (var key in webform) {
-            if (/^(fldset_|fld_|mkup_|authenticated|confirmation_page_path|actions)/.test(key)) {
-              newWebform[key] = webform[key];
-            }
-          }
-          form['webform'] = newWebform;*/
-          form['webform'] = require('./frmDetail'+form.target_id+'.json');
+          var webform = require('./frmDetail'+form.target_id+'.json');
+          form['webform'] = _.deep(webform, function(x) {
+              return _.mapKeys(x, function (val, key) {
+                return key.replace('#','');
+              });
+          });
           allForms[i] = form;
           i++;
           gatherForms();
