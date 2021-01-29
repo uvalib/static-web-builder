8125b44d4e6212a1f97b2ab2d301b03cb96602f9
Doug Chestnut <dougchestnut@gmail.com>
2021-01-29 17:04:52 -0500

    pull temp closed field from drupal directly

--- filter-hours-open.js
+++ filter-hours-open.js
@@ -8,40 +8,48 @@ var now = DateTime.local();
 const serviceAccount = require(process.env.FIREBASEKEY);
 const libcalURL = "https://api3.libcal.com/api_hours_grid.php?format=jsonld&iid=863&lid=";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://uvalib-api.firebaseio.com`
-});
-let app = admin.app();
-const apiDb = app.database();
-const occupancyDb = app.database("https://uvalib-api-occupancy.firebaseio.com/");
 
-occupancyDb.ref('locations-schemaorg/location').once("value",loc=>{
-//  var promises = [];
-  let locations = loc.val();
-  var promises = [];
-  for (const key in locations) {
-    const location = locations[key];
+fetch('https://drupal.lib.virginia.edu/libs?_format=json').then(res=>res.json()).then((d)=>{
 
-    var isOpen = false;
-    // eval if the loc is open currently
-    if (Array.isArray(location.openingHoursSpecification)) {
-      var todays = location.openingHoursSpecification.find(d=>{ 
-        return ( DateTime.fromISO(d.validFrom+'T'+d.opens+':00').toMillis() <= now.toMillis() && now.toMillis() <= DateTime.fromISO(d.validThrough+'T'+d.closes+':00').toMillis() )
-      });
-      // if we have a day, then the location is currently open)
-      if (location.isOpenNow != !!(todays))
-        promises.push( occupancyDb.ref('locations-schemaorg/location/'+key+'/isOpenNow').set(!!(todays)) );
-    }        
-//    promises.push (fetch(libcalURL+hoursKeys[key]).then(res=>res.json())
-//      .then(json=>{
-//        console.log("write hours");
-//        if (json.openingHoursSpecification)
-//          return occupancyDb.ref('locations-schemaorg/location/'+key+'/openingHoursSpecification').set(json.openingHoursSpecification);
-//        else
-//          return Promise.resolve();  
-//      }));
-  }
-  Promise.all(promises).then(()=>process.exit(0)) 
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://uvalib-api.firebaseio.com`
+  });
+  let app = admin.app();
+  const apiDb = app.database();
+  const occupancyDb = app.database("https://uvalib-api-occupancy.firebaseio.com/");
+
+  occupancyDb.ref('locations-schemaorg/location').once("value",loc=>{
+  //  var promises = [];
+    let locations = loc.val();
+    var promises = [];
+    for (const key in locations) {
+      const location = locations[key];
+
+      var drupalLoc = d.find(l=>l['field_slug'][0].value==key);
+      var openagain = false;
+      if (drupalLoc && drupalLoc['field_closure_override'] && drupalLoc['field_closure_override'].length > 0) {
+        openagain = DateTime.fromSeconds( drupalLoc['field_closure_override'][0].value );
+        if (now <= openagain && location.tempClosed != openagain.toMillis())
+          promises.push( occupancyDb.ref('locations-schemaorg/location/'+key+'/tempClosed').set(openagain.toMillis()) );
+      }
+
+      var isOpen = false;
+      // eval if the loc is open currently
+      if (Array.isArray(location.openingHoursSpecification)) {
+        var todays = location.openingHoursSpecification.find(d=>{ 
+          return ( DateTime.fromISO(d.validFrom+'T'+d.opens+':00').toMillis() <= now.toMillis() && now.toMillis() <= DateTime.fromISO(d.validThrough+'T'+d.closes+':00').toMillis() )
+        });
+        // if we have a day, then the location is currently open)
+        if (now <= openagain) todays = false;
+        if (location.isOpenNow != !!(todays))
+          promises.push( occupancyDb.ref('locations-schemaorg/location/'+key+'/isOpenNow').set(!!(todays)) );
+      }        
+
+
+    }
+    Promise.all(promises).then(()=>process.exit(0)) 
+
+  });
 
 });
