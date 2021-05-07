10cba2b0a76dbee823733525bb437cce554635e9
Doug Chestnut <dougchestnut@gmail.com>
2021-05-07 13:53:59 -0400

    fresh start

--- getFeeds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://drupal.lib.virginia.edu/rest/files?_format=json -OrawAPI/rest/files
-wget https://drupal.lib.virginia.edu/pages?_format=json -OrawAPI/pages
-wget https://drupal.lib.virginia.edu/blocks?_format=json -OrawAPI/blocks
-wget https://drupal.lib.virginia.edu/rest/resource_cards?_format=json -OrawAPI/rest/resource_cards
-wget https://drupal.lib.virginia.edu/rest/stages?_format=json -OrawAPI/rest/stages
-rsync -avh -e 'ssh -i $HOME/.ssh/id_rsa' rawAPI/ static01.lib.virginia.edu:/lib_content47/static/rawAPI;
